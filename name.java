1aa4c937e32cc6c0f5062491bf64eb73071a8b56
prasanthi <prasanthi@theretailinsights.com>
2025-06-05 13:02:35 +0530

    modifying changes

--- name.java
+++ name.java
@@ -1,5 +1,6 @@
 public class Name{
     public static void main(){
-        system.out.println("prasanthi");
+        // system.out.println("prasanthi");
+        system.out.println("adc");
     }
 }
